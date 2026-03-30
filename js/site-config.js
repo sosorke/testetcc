94230f02a4c88b861c168d5582478f97f5b1eb02
sosorke <sosorwvk@gmail.com>
2026-03-30 18:47:08 -0300

    Update site-config.js

--- js/site-config.js
+++ js/site-config.js
@@ -1,1 +1,4 @@
-
+window.CLINICA_CONFIG = {
+  googleAppsScriptUrl: "https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Prata&display=swap" rel="stylesheet",
+  googleSheetUrl: ""
+};
